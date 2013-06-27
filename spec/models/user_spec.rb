1e6e6aca131394716c02f61cef21621fc4eba54c
xinranw <xinranw@seas.upenn.edu>
2013-06-26 23:25:35 -0400

    Fixed user and user_spec errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,7 @@ require 'spec_helper'
 
 describe User do
   before do
-    @user = User.new(name: "Example User", email: "user.example.com", password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
@@ -72,6 +72,16 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@eXamPle.Com"}
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " "}
     it { should_not be_valid}
@@ -87,6 +97,11 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
+
   describe "return values of authenticate method" do
     before { @user.save }
     let(:found_user) { User.find_by_email(@user.email) }
@@ -94,16 +109,12 @@ describe User do
     describe "with valid password" do
       it { should == found_user.authenticate(@user.password) }
     end
+
     describe "with invalid password" do
       let(:user_for_invalid_password) { found_user.authenticate("invalid") }
 
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false}
     end
-  end
-
-  describe "with a password that's too short" do
-    before { @user.password = @user.password_confirmation = "a" * 5 }
-    it { should be_invalid }
   end
 end
